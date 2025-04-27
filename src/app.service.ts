ca35b20a37002e55f1e370ee230ed7378752d550
Farjad Ahmed <farjad.ahmed@whetstonez.com>
2025-04-27 19:09:48 +0500

    fix: issue

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, how are you, you are from?!';
+    return 'Hello World, how are you, you are from, Zuha ji ?!';
   }
 }
